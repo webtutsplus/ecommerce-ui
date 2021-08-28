05222db2845db34318e440908f31f16b9a174ceb
giridhar30 <giriit30@gmail.com>
2021-08-28 21:14:39 +0530

    products displayed

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import AddCategory from "../views/Category/AddCategory";
 import Category from "../views/Category/Category";
 import EditCategory from "../views/Category/EditCategory";
 import AddProduct from "../views/Product/AddProduct";
+import Product from "../views/Product/Product";
 
 Vue.use(VueRouter);
 
@@ -43,6 +44,11 @@ const routes = [
     path: "/admin/product/add",
     name: "AddProduct",
     component: AddProduct,
+  },
+  {
+    path: "/admin/product",
+    name: "AdminProduct",
+    component: Product,
   },
 ];
 
