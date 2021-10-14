3a40f2925558b819e080c381425341dd115be823
nmadhab <nilmadhab.mondal@booking.com>
2021-10-14 18:21:48 +0200

    delete cart implemented

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -23,6 +23,8 @@
             Quantity :
             <input size="1" class="p-0 h-25 border-bottom border-top-0 border-left-0 border-right-0" v-model="cartItem.quantity" /></p>
           <p id="item-total-price" class="mb-0">Total : <span class="font-weight-bold"> $ {{cartItem.product.price*cartItem.quantity}}</span></p>
+          <!--       1. make a function deleteItem and pass cartitem.id -->
+          <br><a href="#" class="text-right" @click="deleteItem(cartItem.id)">Remove From Cart</a>
         </div>
       </div>
       <div class="col-2"></div>
@@ -63,12 +65,25 @@ export default {
           const result = response.data;
           // store cartitems and total price in two variables
           this.cartItems = result.cartItems;
-          this.totalcost = result.totalCost
+          this.totalcost = result.totalCost.toFixed(2);
         }
       },
       (error)=>{
         console.log(error)
       });
+    },
+    //delete the cart item
+    deleteItem(itemId) {
+       // 2. first delete the item by calling delete api
+      axios.delete(`${this.baseURL}cart/delete/${itemId}/?token=${this.token}`)
+        .then((response)=>{
+          if(response.status == 200){
+            // 3. refresh the data by calling listCartItems
+            this.listCartItems();
+          }
+        },(error)=>{
+          console.log(error)
+        })
     }
   },
 
