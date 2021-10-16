6c4844717a8fb6782b6e55c8a10266a7ead554fc
nmadhab <nilmadhab.mondal@booking.com>
2021-10-16 22:49:10 +0200

    order details task solution implemented

--- src/views/order/OrderDetails.vue
+++ src/views/order/OrderDetails.vue
@@ -7,13 +7,37 @@
         </div>
 
         <!-- TODO       Loop through orderItems and display each item-->
+        <div v-for="(orderItem, index) in orderItems" :key="index" class="row mt-2 pt-3 justify-content-around">
+            <!--            leave some padding in left-->
+            <div class="col-1"></div>
+            <!--            Display image-->
+            <div class="col-md-3 embed-responsive embed-responsive-16by9">
+                <img :src="orderItem.product.imageURL" class="w-100 card-img-top embed-responsive-item">
+                <hr/>
+            </div>
+            <!--            Display order item informations-->
+            <div class="col-md-5 px-3">
+                <div class="card-block px-3">
+                    <h6 class="card-title" >{{orderItem.product.name}}</h6>
+                    <p id="item-price" class="mb-0 font-weight-bold">${{orderItem.product.price}} per unit</p>
+                    <p id="item-quantity" class="mb-0">Quantity : {{orderItem.quantity}}</p>
+                    <p id="item-total-price" class="mb-0">
+                        Total Price : $<span class="font-weight-bold">{{ orderItem.price * orderItem.quantity}}</span>
+                    </p>
+                </div>
+            </div>
+            <div class="col-1"></div>
+        </div>
 
         <!--        Display the total price-->
+        <div class="total-cost pt-2 text-right">
+            <h5>Total Cost : $ {{order.totalPrice}}</h5>
+        </div>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
     name:'OrderItems',
     props:["baseURL"],
@@ -30,12 +54,18 @@ export default {
     methods:{
         getOrder(){
             // TODO 1. fetch api `/order/${orderID}?token=$token}`
-
             // save response into order and order items
+            axios.get(`${this.baseURL}order/${this.orderID}?token=${this.token}`).then((response) => {
+                    if(response.status === 200) {
+                        this.order = response.data
+                        this.orderItems = this.order.orderItems
+                    }
+                },
+                (err)=>{
+                    console.log(err)
+                })
         }
-
     },
-
     mounted(){
         // get the id in param
         this.orderID = this.$route.params.id;
