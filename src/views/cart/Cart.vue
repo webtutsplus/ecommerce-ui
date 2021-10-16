2cc1af5f8da33bc8264fbf97e42f1b4ad6e11036
nmadhab <nilmadhab.mondal@booking.com>
2021-10-16 16:27:39 +0200

    delete cart item soln

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -24,7 +24,7 @@
             <input size="1" class="p-0 h-25 border-bottom border-top-0 border-left-0 border-right-0" v-model="cartItem.quantity" /></p>
           <p id="item-total-price" class="mb-0">Total : <span class="font-weight-bold"> $ {{cartItem.product.price*cartItem.quantity}}</span></p>
           <!--       1. make a function deleteItem and pass cartitem.id -->
-          <br><a href="#" class="text-right" @click="deleteItem(cartItem.id)">Remove From Cart</a>
+          <br><a href="#" @click="deleteItem(cartItem.id)">Remove From Cart</a>
         </div>
       </div>
       <div class="col-2"></div>
